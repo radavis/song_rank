5a59a416da9a66bbaa718f2415de34bd4c18c2f1
Richard Davis <rdavis.bacs@gmail.com>
2013-10-18 09:06:28 -0400

    ratings are saved and updated

--- app/assets/javascripts/albums.js
+++ app/assets/javascripts/albums.js
@@ -1,9 +1,12 @@
 $(document).ready(function() {
+  // clickable rows on /albums/ page
   $('tr.album').on('click', function() {
     window.location = '/albums/' + $(this).attr('id');
   });
 
+  // update a track's rating
   $('div.rating').raty({
+    score: function() { return $(this).attr('score'); },
     path: "/assets",
     click: function(score, e) {
       $.ajax({
